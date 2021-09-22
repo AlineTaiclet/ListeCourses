1d1a6c5b342d300c6d57daee4d2ec1d641e97d6c
Aline Taiclet <aline.taiclet@gmail.com>
2021-09-22 17:48:38 +0200

    ajout

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,23 +3,44 @@
 namespace App\Controller;
 
 use App\Entity\Item;
+use App\Form\ItemType;
 use App\Repository\ItemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(ItemRepository $repo): Response
+    public function home(ItemRepository $repo, Request $request): Response
     {
 
         $items = $repo->findAll();
+        //dd($items);
+        $item = new Item();
 
-        return $this->render('main/index.html.twig', [
-            'items' => $items ,
+        //associe objet item au form
+        $formItem = $this->createForm(ItemType::class,$item);
+
+        //hydrater item
+        $formItem->handleRequest($request);
+
+        if($formItem->isSubmitted()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($item);
+            $em->flush();
+            // je redirige
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('main/index.html.twig',[
+            'formItem' => $formItem->createView(),
+            'items' => $items
         ]);
+
     }
 }
